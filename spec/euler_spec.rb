e8947425101d05639be3e9300b7156bfdc85aac1
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-09-16 16:45:15 +0100

    Problem 1 done

--- spec/euler_spec.rb
+++ spec/euler_spec.rb
@@ -1,5 +1,17 @@
 require "./euler.rb"
 
-describe "something" do
- it "something_else"
+describe Euler do
+
+  context "problem_1" do
+    it "should return a kind of integer" do
+      euler = Euler.new
+      expect(euler.problem_1(10)).to be_a_kind_of(Integer)
+    end
+
+    it "sums the multiples of 3 and 5 under 10" do
+      euler = Euler.new
+      expect(euler.problem_1(10)).to eq(23)
+    end
+  end
+
 end
